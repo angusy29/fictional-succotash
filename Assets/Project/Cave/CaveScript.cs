f4d7a8617f484dfd543fbafaafd73fbe66e6ac4b
Angus Yuen <angus.yuen29@gmail.com>
2017-01-08 13:35:10 +1100

    CaveEnterUI is now a prefab, added portal to arcane sanctuary

--- Assets/Project/Cave/CaveScript.cs
+++ Assets/Project/Cave/CaveScript.cs
@@ -7,8 +7,10 @@ using UnityEngine.SceneManagement;
 public class CaveScript : MonoBehaviour {
 
 	public GameObject caveEnterUI;
-	public GameObject actionButton;
-	public GameObject caveNameText;
+	// public GameObject actionButton;
+	// public GameObject caveNameText;
+
+	private GameObject userInterfaceLabel;	// this is the actual label that user sees
 
 	// private Color lightOrange = new Color (0.96f, 0.77f, 0.42f);
 	private Color lightBlue = new Color(0.5f, 0.8f, 1.0f);
@@ -17,31 +19,36 @@ public class CaveScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		caveEnterUI.SetActive (false);
+		userInterfaceLabel = (GameObject)Instantiate (caveEnterUI);
+
 		lerpedColor = lightBlue;
 		print (this.name);
-		caveNameText.GetComponent<Text>().text = this.name;
+
+		userInterfaceLabel.transform.FindChild ("CaveName").GetComponent<Text> ().text = this.name;
+
+		userInterfaceLabel.SetActive (false);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (caveEnterUI.activeSelf) {
+		if (userInterfaceLabel.activeSelf) {
 			lerpedColor = Color.Lerp (Color.white, lightBlue, Mathf.PingPong (Time.time, 1));
-			actionButton.GetComponent<RawImage> ().color = lerpedColor;
-		
+			// actionButton.GetComponent<RawImage> ().color = lerpedColor;
+
+			userInterfaceLabel.transform.FindChild ("ActionButton").GetComponent<RawImage> ().color = lerpedColor;
 		
 			if (Input.GetKeyDown (KeyCode.E)) {
 				print ("User entered cave!");
-				SceneManager.LoadScene ("Scenes/" + caveNameText.GetComponent<Text>().text);
+				SceneManager.LoadScene ("Scenes/" + this.name);
 			}
 		}
 	}
 
 	void OnTriggerEnter(Collider other) {
-		caveEnterUI.SetActive (true);
+		userInterfaceLabel.SetActive (true);
 	}
 
 	void OnTriggerExit(Collider other) {
-		caveEnterUI.SetActive (false);
+		userInterfaceLabel.SetActive (false);
 	}
 }
