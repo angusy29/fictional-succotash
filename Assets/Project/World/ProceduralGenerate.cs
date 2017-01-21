2e3853a6188d39b2e388890e15f3896e6fe3554a
Angus Yuen <angus.yuen29@gmail.com>
2017-01-22 10:24:54 +1100

    Adding prefabs for arcane sanctuary and starting on proceduralGenerate.cs

---
+++ Assets/Project/World/ProceduralGenerate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGenerate : MonoBehaviour {
+	public GameObject corridor;
+	public GameObject junction;
+	public GameObject link;
+	public GameObject room;
+	public GameObject slope;
+
+	private List<GameObject> platforms_;
+
+	// Use this for initialization
+	void Start () {
+		platforms_ = new List<GameObject> ();
+		platforms_.Add (corridor);
+		platforms_.Add (junction);
+		platforms_.Add (link);
+		platforms_.Add (room);
+		platforms_.Add (slope);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
