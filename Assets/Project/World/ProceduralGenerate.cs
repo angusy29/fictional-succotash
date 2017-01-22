9d5fa8dbed4993dd259a35c23332ef488cbf44ca
Angus Yuen <angus.yuen29@gmail.com>
2017-01-22 11:42:04 +1100

    Can now randomly generate a dungeon, have not added in random rotation

--- Assets/Project/World/ProceduralGenerate.cs
+++ Assets/Project/World/ProceduralGenerate.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ProceduralGenerate : MonoBehaviour {
+	public GameObject startPlatform;
 	public GameObject corridor;
 	public GameObject junction;
 	public GameObject link;
 	public GameObject room;
 	public GameObject slope;
 
-	private List<GameObject> platforms_;
+	public int numIterations;
+
+	private List<GameObject> platforms_;	// keeps track of available prefabs
+	private GameObject currPlatform_;		// keeps track of the current platform
 
 	// Use this for initialization
 	void Start () {
@@ -19,10 +23,37 @@ public class ProceduralGenerate : MonoBehaviour {
 		platforms_.Add (link);
 		platforms_.Add (room);
 		platforms_.Add (slope);
+
+		currPlatform_ = startPlatform;
+
+		for (int i = 0; i < numIterations; i++) {
+			currPlatform_ = GenerateDungeon (currPlatform_);
+		}
+	}
+
+	// adds one platform to the dungeon and returns the new platform
+	GameObject GenerateDungeon(GameObject currPlatform) {
+
+		// pick a random exit of the current platform
+		Transform exit = currPlatform.transform;	// for now just pick the first exit
+		foreach (Transform child in currPlatform.transform) {
+			if (child.CompareTag ("Exit")) {
+				exit = child;
+			}
+		}
+
+		// instantiate a new gameobject
+		int randIndex = Random.Range (0, platforms_.Count);
+		// randomly rotate the object
+		currPlatform = (GameObject) Instantiate (platforms_[randIndex], new Vector3(exit.position.x, exit.position.y, exit.position.z), Quaternion.identity);
+
+		// change current platform
+
+		return currPlatform;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	/* void Update () {
 		
-	}
+	} */
 }
